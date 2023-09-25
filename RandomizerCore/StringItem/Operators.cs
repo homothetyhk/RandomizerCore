3ccda39dd3f6d1ccb636be81d2c0a8db6bcfd2e1
Sean Dempsey <dempsey.sean@outlook.com>
2023-09-24 17:16:40 -0700

    Fix associativity of conditional operator

--- RandomizerCore/StringItem/Operators.cs
+++ RandomizerCore/StringItem/Operators.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -57,18 +58,28 @@ namespace RandomizerCore.StringItem
     public static class Operators
     {
         // infix operators
+        // Term, Int -> Effect. Adds the provided value to the term
         public const string AdditionAssignment = "+=";
+        // Term, Int -> Effect. Sets the term to the maximum of the provided value and its current value
         public const string MaxAssignment = "=/";
+        // Bool, Effect -> Effect. If the condition is met, applies the effect
         public const string Conditional = "=>";
+        // Effect, Effect -> Effect. Applies the right-hand effect if and only if the left-hand effect produces an empty item
         public const string ShortCircuitChaining = ">|>";
+        // Effect, Effect -> Effect. Applies the 
         public const string Chaining = ">>";
 
         // prefix operators
+        // where a bool is expected, the string is interpreted as the name of a logic def and its value is used as a bool.
+        // where an item effect is expected, the string is interpreted as the name of an item and its effect is copied
         public const string Reference = "*";
+        // Bool -> Bool. Negates the bool
         public const string Negation = "!";
 
         // postfix operators
+        // if the associated term does not correspond to a term in the LogicManager, any effect that would be applied to that term is ignored
         public const string TermCoalescing = "?";
+        // Term -> Effect. Adds 1 to the term.
         public const string Increment = "++";
 
         public static readonly IReadOnlyCollection<string> AllOperators = new HashSet<string>()
@@ -106,7 +117,7 @@ namespace RandomizerCore.StringItem
         {
             Chaining => (1, 2),
             ShortCircuitChaining => (3, 4),
-            Conditional => (5, 6),
+            Conditional => (6, 5),
             MaxAssignment or AdditionAssignment => (7, 8),
             _ => throw new NotImplementedException()
         };
--- RandomizerCoreTests/ItemParserTests.cs
+++ RandomizerCoreTests/ItemParserTests.cs
@@ -93,73 +93,103 @@ namespace RandomizerCoreTests
         }
 
         [Fact]
+        public void TestParserAssociativity()
+        {
+            // string input = "`Grubsong = 0` => `Grubsong < 1` => CHARMS++ >> Grubsong++";
+            List<Token> tokens = new()
+            {
+                new LogicStringToken { Value = "Grubsong = 0", LeadingTrivia = "`", TrailingTrivia = "` ", StartCharacter = 1, EndCharacter = 12 },
+                new OperatorToken { Operator = "=>", LeadingTrivia = "", TrailingTrivia = " ", StartCharacter = 15, EndCharacter = 16},
+                new LogicStringToken { Value = "Grubsong < 1", LeadingTrivia = "`", TrailingTrivia = "` ", StartCharacter = 19, EndCharacter = 30 },
+                new OperatorToken { Operator = "=>", LeadingTrivia = "", TrailingTrivia = " ", StartCharacter = 33, EndCharacter = 34 },
+                new NameToken { Value = "CHARMS", LeadingTrivia = "", TrailingTrivia = "", StartCharacter = 36, EndCharacter = 41 },
+                new OperatorToken { Operator = "++", LeadingTrivia = "", TrailingTrivia = " ", StartCharacter = 42, EndCharacter = 43 },
+                new OperatorToken { Operator = ">>", LeadingTrivia = "", TrailingTrivia = " ", StartCharacter = 45, EndCharacter = 46 },
+                new NameToken { Value = "Grubsong", LeadingTrivia = "", TrailingTrivia = "", StartCharacter = 48, EndCharacter = 55 },
+                new OperatorToken { Operator = "++", LeadingTrivia = "", TrailingTrivia = "", StartCharacter = 56, EndCharacter = 57 } 
+            };
+
+            ItemParser parser = new(tokens);
+            IExpression expr = parser.Parse();
+            expr.Should().BeEquivalentTo(new ChainingExpression(
+                new ConditionalExpression(
+                    new AtomExpression(tokens[0]),
+                    (OperatorToken)tokens[1],
+                    new ConditionalExpression(
+                        new AtomExpression(tokens[2]),
+                        (OperatorToken)tokens[3],
+                        new IncrementExpression(
+                            new AtomExpression(tokens[4]),
+                            (OperatorToken)tokens[5]
+                        )
+                    )
+                ),
+                (OperatorToken)tokens[6],
+                new IncrementExpression(
+                    new AtomExpression(tokens[7]),
+                    (OperatorToken)tokens[8]
+                )
+            ));
+            expr.Validate().Should().BeTrue("this is a valid expression");
+            expr.Evaluate().Should().BeEquivalentTo(new[] { EvaluatedType.ItemEffect });
+            expr.Print().Should().Be("`Grubsong = 0` => `Grubsong < 1` => CHARMS++ >> Grubsong++");
+        }
+
+        [Fact]
         public void TestParserParens()
         {
-            List<Token> tokens = new ItemTokenizer("(((Grubsong)))").Tokenize();
-            IExpression expr = new ItemParser(tokens).Parse();
-            expr.Should().BeEquivalentTo(new GroupingExpression(
+            // string input = "(((Grubsong)))";
+            List<Token> tokens = new()
+            {
                 new StructuralToken
                 {
-                    StartCharacter = 0,
-                    EndCharacter = 0,
-                    LeadingTrivia = "",
-                    TrailingTrivia = "",
+                    StartCharacter = 0,  EndCharacter = 0, LeadingTrivia = "", TrailingTrivia = "",
                     TokenType = StructuralToken.Type.OpenParenthesis
                 },
-                new GroupingExpression(
-                    new StructuralToken
-                    {
-                        StartCharacter = 1,
-                        EndCharacter = 1,
-                        LeadingTrivia = "",
-                        TrailingTrivia = "",
-                        TokenType = StructuralToken.Type.OpenParenthesis
-                    },
-                    new GroupingExpression(
-                        new StructuralToken
-                        {
-                            StartCharacter = 2,
-                            EndCharacter = 2,
-                            LeadingTrivia = "",
-                            TrailingTrivia = "",
-                            TokenType = StructuralToken.Type.OpenParenthesis
-                        },
-                        new AtomExpression(
-                            new NameToken
-                            {
-                                StartCharacter = 3,
-                                EndCharacter = 10,
-                                LeadingTrivia = "",
-                                TrailingTrivia = "",
-                                Value = "Grubsong"
-                            }
-                        ),
-                        new StructuralToken
-                        {
-                            StartCharacter = 11,
-                            EndCharacter = 11,
-                            LeadingTrivia = "",
-                            TrailingTrivia = "",
-                            TokenType = StructuralToken.Type.CloseParenthesis
-                        }
-                    ),
-                    new StructuralToken
-                    {
-                        StartCharacter = 12,
-                        EndCharacter = 12,
-                        LeadingTrivia = "",
-                        TrailingTrivia = "",
-                        TokenType = StructuralToken.Type.CloseParenthesis
-                    }
-                ),
                 new StructuralToken
                 {
-                    StartCharacter = 13,
-                    EndCharacter = 13,
-                    LeadingTrivia = "",
-                    TrailingTrivia = "",
+                    StartCharacter = 1, EndCharacter = 1, LeadingTrivia = "", TrailingTrivia = "",
+                    TokenType = StructuralToken.Type.OpenParenthesis
+                },
+                new StructuralToken
+                {
+                    StartCharacter = 2, EndCharacter = 2, LeadingTrivia = "", TrailingTrivia = "",
+                    TokenType = StructuralToken.Type.OpenParenthesis
+                },
+                new NameToken
+                {
+                    StartCharacter = 3, EndCharacter = 10, LeadingTrivia = "", TrailingTrivia = "",
+                    Value = "Grubsong"
+                },
+                new StructuralToken
+                {
+                    StartCharacter = 11, EndCharacter = 11, LeadingTrivia = "", TrailingTrivia = "",
+                    TokenType = StructuralToken.Type.CloseParenthesis
+                },
+                new StructuralToken
+                {
+                    StartCharacter = 12, EndCharacter = 12, LeadingTrivia = "", TrailingTrivia = "",
+                    TokenType = StructuralToken.Type.CloseParenthesis
+                },
+                new StructuralToken
+                {
+                    StartCharacter = 13, EndCharacter = 13, LeadingTrivia = "", TrailingTrivia = "",
                     TokenType = StructuralToken.Type.CloseParenthesis
                 }
+            };
+            IExpression expr = new ItemParser(tokens).Parse();
+            expr.Should().BeEquivalentTo(new GroupingExpression(
+                (StructuralToken)tokens[0],
+                new GroupingExpression(
+                    (StructuralToken)tokens[1],
+                    new GroupingExpression(
+                        (StructuralToken)tokens[2],
+                        new AtomExpression(tokens[3]),
+                        (StructuralToken)tokens[4]
+                    ),
+                    (StructuralToken)tokens[5]
+                ),
+                (StructuralToken)tokens[6]
             ));
             expr.Validate().Should().BeTrue("this is a valid expression, just not a valid item effect");
             expr.Evaluate().Should().BeEquivalentTo(new[] { EvaluatedType.TermLike, EvaluatedType.Bool });
