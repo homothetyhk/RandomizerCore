7add80ac1645bceef31cb34e9cf4019040191f25
Sean Dempsey <dempsey.sean@outlook.com>
2023-12-10 14:07:15 -0800

    Support .NET 8

--- RandomizerCore/StringParsing/Exceptions.cs
+++ RandomizerCore/StringParsing/Exceptions.cs
@@ -6,9 +6,12 @@
         public TokenizingException() { }
         public TokenizingException(string message) : base(message) { }
         public TokenizingException(string message, Exception inner) : base(message, inner) { }
+
+#if !NET8_0_OR_GREATER
         protected TokenizingException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+#endif
     }
 
     [Serializable]
@@ -17,8 +20,11 @@
         public ParsingException() { }
         public ParsingException(string message) : base(message) { }
         public ParsingException(string message, Exception inner) : base(message, inner) { }
+
+#if !NET8_0_OR_GREATER
         protected ParsingException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+#endif
     }
 }
